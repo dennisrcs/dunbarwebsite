ae4357a9ec20a3cd7c93975c858a4dbe9dc6847f
dmartinez05 <dmartinez05@tamu.edu>
2016-03-31 15:54:01 +0000

    static_page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+ 
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash.now[:success] = 'Success login'
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+  
+end
