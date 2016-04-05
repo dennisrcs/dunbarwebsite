750a0e0f912652e70d42e0c085659c870a7016c7
dmartinez05 <dmartinez05@tamu.edu>
2016-04-05 23:11:11 +0000

    Fixing heroku

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -2,7 +2,7 @@ class Publication < ActiveRecord::Base
   
   attr_accessible :article, :contributors, :journal, :year, :more_info, :pages, :link#, :publication_picture
   
-  mount_uploader :publication_picture, Publication_pictureUploader
+  #mount_uploader :publication_picture, Publication_pictureUploader
   
   #insert more details of model
   
