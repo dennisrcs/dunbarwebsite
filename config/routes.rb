a8f5f6b39a90d42a698951cec6b0ddc011bd15c1
dmartinez05 <dmartinez05@tamu.edu>
2016-04-05 16:57:35 +0000

    Adding create publication and model. Controller Missing

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   
-  get     'publications' => 'publications#index'
-  
+  #get     'publications' => 'publications#index'
+  #get    'publications/new' => 'publications#new'
   resources :publications, only: [:index, :new]
   
   #resources :resumes, only: [:index, :new, :create, :destroy]
