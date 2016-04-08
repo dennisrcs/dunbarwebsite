83413c683ecc8d08d5b9e77badccc0982ea06bf9
Irving Solis <g02005760@tamu.edu>
2016-04-08 23:12:22 +0000

    added new_member fueature file

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160406102002) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
