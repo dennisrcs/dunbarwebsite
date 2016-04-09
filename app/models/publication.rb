9edc088174630746306612827856324baa9dd96a
dmartinez05 <dmartinez05@tamu.edu>
2016-04-09 02:48:26 +0000

    Publication finished

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,14 +1,5 @@
 class Publication < ActiveRecord::Base
-<<<<<<< 750a0e0f912652e70d42e0c085659c870a7016c7
-  
-  attr_accessible :article, :contributors, :journal, :year, :more_info, :pages, :link#, :publication_picture
-  
-  #mount_uploader :publication_picture, Publication_pictureUploader
-  
-  #insert more details of model
-=======
   attr_accessible :article, :contributors, :journal, :year, :more_info, :pages, :link, :publication_picture
->>>>>>> Adding edit Publication fixing minor bugs
   
   # writes 'data' to the public folder, following 'path' structure
   def self.write_to_filesystem(data, path)
