f0a6fdfac8578d35deb148df22d5b4fbd5095b74
bozhen_liu <bozhen.liu1989@gmail.com>
2016-04-26 18:04:24 +0000

    adding password resets controller spec

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-migrate-min
 //= require turbolinks
 //= require_tree .
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  config.assets.precompile += %w( jquery-1.7.js )
+    config.action_mailer.default_url_options = {:host => "localhost:3000"}
 
-  
 end
