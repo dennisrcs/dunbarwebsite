24385054e5af881713436dedf8fd31b179a1db23
duwacu <dudiannahu@gmail.com>
2016-05-02 23:12:06 +0000

    adding all files for homeslider branch

--- db/migrate/20160430220253_create_slider_images.rb
+++ db/migrate/20160430220253_create_slider_images.rb
@@ -3,7 +3,7 @@ class CreateSliderImages < ActiveRecord::Migration
     create_table :slider_images do |t|
       t.string :title
       t.text :link
-      t.string :slider_image_picture
+      t.text :slider_image_picture
 
       t.timestamps null: false
     end
--- spec/models/slider_image_spec.rb
+++ spec/models/slider_image_spec.rb
@@ -1,5 +1,23 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe SliderImage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe SliderImage do
+    before :each do
+        @pattr = {
+            :title => "some article", 
+            :link =>"uploads/articles/article1.pdf", 
+            :sliderImage_picture =>"/uploads/slider/HAT.png"
+        }
+        @p = SliderImage.create!(@pattr)
+    end
+    it 'should create a valid SliderImage type' do
+        #created SliderImage is_an_instance_of SliderImage
+        @p.should(be_an_instance_of(SliderImage))
+    end
+    
+    it 'should write data to the file system' do
+       return_value = "/uploads/slider/HAT.png"
+       @path = SliderImage.send(:write_to_filesystem,@p[:sliderImage_picture],  "/uploads/slider")
+       @path =~ return_value
+    end
+    
 end
