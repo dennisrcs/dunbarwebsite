db8d0c7f07db4989e52f7dcf61aefed2288affb8
Dennis Silva <denisrcs@gmail.com>
2016-05-04 05:41:05 +0000

    Adding show action to add course feature

--- app/models/course_file.rb
+++ app/models/course_file.rb
@@ -1,6 +1,6 @@
 class CourseFile < ActiveRecord::Base
-  attr_accessible :name, :file_path, :type
-  enum type: [ :misc, :syllabus, :notes, :chapter_goals, :resource_research, :homework, :solutions, :exam_answers ]
+  attr_accessible :name, :file_path, :file_type
+  enum file_type: [ :misc, :syllabus, :notes, :chapter_goals, :resource_research, :homework, :solutions, :exam_answers ]
 
   validates :name, presence: true
   validates :file_path, presence: true
