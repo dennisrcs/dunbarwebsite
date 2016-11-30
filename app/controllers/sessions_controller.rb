5ba9430b7917ec9a37353771694a6129db7bdcf2
Dennis Silva <denisrcs@gmail.com>
2016-11-30 21:24:47 +0000

    Changing session#create. Removing page error redirection in development. Adding verify_authenticity_token to skip_before_filter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:session][:username].downcase)
-    if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        flash[:info] = "Welcome #{user.username}!"
-        redirect_to root_path
+    if params[:session] != nil
+      user = User.find_by(username: params[:session][:username].downcase)
+      if user && user.authenticate(params[:session][:password])
+        if user.activated?
+          log_in user
+          flash[:info] = "Welcome #{user.username}!"
+          redirect_to root_path
+        else
+          message  = "Account not activated. "
+          message += "Check your email for the activation link."
+          flash[:danger] = message
+          redirect_to root_url
+        end
       else
-        message  = "Account not activated. "
-        message += "Check your email for the activation link."
-        flash[:danger] = message
-        redirect_to root_url
+        flash.now[:danger] = 'Invalid username/password combination'
+        render 'new'
       end
     else
-      flash.now[:danger] = 'Invalid username/password combination'
+      flash.now[:danger] = 'Something went wrong. Please try again.'
       render 'new'
     end
   end
